90b1d2fc58810936fc41ed5414d1fe60e913f085
udeshya17 <udeshyasharma17@gmail.com>
2024-07-28 00:25:48 +0530

    correction done

--- src/App.js
+++ src/App.js
@@ -8,59 +8,54 @@ function App() {
   const [error, setError] = useState('');
 
   const handleUsername = (e) => {
-    setUsername(e.target.value);
+    setUsername(username => e.target.value);
+    console.log(username)
   }
 
   const handlePassword = (e) => {
-    setPassword(e.target.value);
+    setPassword(password => e.target.value);
+    console.log(password);
   }
 
   const handleResult = (e) => {
     e.preventDefault();
-    if (username === "" || password === "") {
-      setError("Username and password cannot be empty");
-      return;
-    }
     if (username === "user" && password === "password") {
       setResult(true);
       setError('');
     } else {
       setError("Invalid username or password");
+      setResult(false)
     }
   }
 
   return (
     <div>
       <h1>Login Page</h1>
-      {(!result) ? (
+      {(result) ? (
+        <div>
+          <p>Welcome, {username}!</p>
+        </div>
+      ):(
         <form onSubmit={handleResult}>
-          {error && <p>{error}</p>}
-          <label>
-            Username:
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+          <label>Username:</label>
             <input
               type="text"
               required
               onChange={handleUsername}
               value={username}
-              placeholder="username"
             />
-          </label>
           <br/>
-          <label>
-            Password:
+          <label>Password:</label>
             <input
               type="password"
               required
               onChange={handlePassword}
               value={password}
-              placeholder="password"
             />
-          </label>
           <br/>
           <button type="submit">Submit</button>
         </form>
-      ) : (
-        <p>Welcome, user!</p>
       )}
     </div>
   );
