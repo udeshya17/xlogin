ddf0ea48d5427d76d8aae5fe2a881d758738c1a0
udeshya17 <udeshyasharma17@gmail.com>
2024-07-27 21:00:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [result, setResult] = useState(false);
+  const [output, setOutput] = useState('')
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const handleResult = (e) => {
+    e.preventDefault();
+    if (username === "user" && password === "password") {
+      setResult(true);
+    } else {
+      setOutput(output => "Invalid username or password")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Login Page</h1>
+      {(!result) ? (
+        <div> {output}
+        <br/>
+        <form onSubmit={handleResult}>
+          <label>
+            Username:
+            <input
+              type="text"
+              required
+              onChange={handleUsername}
+              value={username}
+              placeholder="username"
+            />
+          </label>
+          <br/>
+          <label>
+            Password:
+            <input
+              type="password"
+              required
+              onChange={handlePassword}
+              value={password}
+              placeholder="password"
+            />
+          </label>
+          <br/>
+          <button type="submit">Submit</button>
+        </form>
+        </div>
+      ) : (
+        <p>Welcome, user!</p>
+      )}
     </div>
   );
 }
